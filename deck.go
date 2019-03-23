e8bdeb97617261fe55c02f79fbd41942d77aa052
Dani Santos <santos.adaniele@gmail.com>
2019-03-23 14:04:02 +0100

    adding fruits slice

--- deck.go
+++ deck.go
@@ -9,6 +9,13 @@ type deck []string
 // this borrows the behavior of a slice of strings
 //like extends in OOP
 
+// receiver
+// by CONVENTION, the receiver is referred with
+// a one or two-letter abbreviation that matches the
+// type of the receiver
+// because our receiver is of type deck, we use "d" (the
+//first letter of deck)
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 func main() {
 	cards := deck {"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
-
+	fruits := deck {"Apple", "Banana", "Orange"}
 	cards.print()
+	fruits.print()
 }
 
 func newCard() string {
