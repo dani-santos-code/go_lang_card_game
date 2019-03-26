b2bfadbb6b3299e56bdd53355bd705a41ab3983e
Dani Santos <santos.adaniele@gmail.com>
2019-03-26 14:38:22 +0100

    save a file to harddrive

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+	"os"
+)
 // Create a new type of 
 // Deck which is a slice of strings
 
@@ -19,7 +24,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck {}
 	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Jack", "Queen", "King"}
 
 	for _, suit := range(cardSuits) {
 		for _, value := range(cardValues){
@@ -29,8 +34,35 @@ func newDeck() deck {
 	return cards
 }
 
-func (d deck) print() {
-	for _, card := range d {
-		fmt.Println(card)
+func (d deck) print(){
+	for i, card := range d {
+		fmt.Println(i, card)
 	} 
+}
+
+//returns multiple values from one function
+// returns two values of type deck
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+} 
+
+func (d deck) toString() string {
+	// converting a deck type into a slice of strings (converting back to what it was)
+	return strings.Join([] string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	// error handling
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
